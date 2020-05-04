cf83a1e2d0ebb132ae5247edd13a3b796ab6831a
Srikanth kante <kantesrikanth@gmail.com>
2020-05-04 20:08:11 +0530

    using middlewares

--- testback/index.js
+++ testback/index.js
@@ -7,5 +7,21 @@
 
 const express = require("express");
 const app = express();
+
 app.listen(3000, () => console.log("Port listening . . . ... "));
+
 app.get("/", (req, res) => res.send("welcome"));
+
+const isAdminRole = true;
+const isAdmin = (req, res, next) => {
+  if (isAdminRole) {
+    console.log("isAdminRole", isAdminRole);
+    next();
+  } else {
+    console.log("You dont have access to Admin dashboard");
+  }
+};
+const adminRoute = (req, res) => {
+  return res.send("Welcome to Admin dashboard");
+};
+app.get("/admin", isAdmin, adminRoute);
