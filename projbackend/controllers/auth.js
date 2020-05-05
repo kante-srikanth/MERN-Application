0ddd0e048f1ec7dd768d8e2aa3a460ec5baa8a23
Srikanth kante <kantesrikanth@gmail.com>
2020-05-05 09:01:24 +0530

    signup feature

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,15 @@
+const User = require("../models/user");
+
 exports.signup = (req, res) => {
-  console.log("REQ BODY", req.body);
-  res.json({ message: "user signup success" });
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NOT able to save the user in the DB",
+      });
+    }
+    res.send(user);
+  });
 };
 
 exports.signout = (req, res) => {
