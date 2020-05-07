78f863bb799b5fb984bad123e13addcac57af3b1
Srikanth kante <kantesrikanth@gmail.com>
2020-05-07 09:39:54 +0530

    error validations using express

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,16 +1,21 @@
 //import statements
 const User = require("../models/user");
+const { validationResult } = require("express-validator");
 
 //signup
 exports.signup = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ error: errors.array()[0].msg });
+  }
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
-      return res.status(400).json({
-        err: "NOT able to save the user in the DB",
-      });
+      return res
+        .status(400)
+        .json({ err: "Please try after sometime or try with new details" });
     }
-    res.send(user);
+    res.json({ name: user.name, email: user.email });
   });
 };
 
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,10 +1,20 @@
 //import statements
 var express = require("express");
 var router = express.Router();
+const { check } = require("express-validator");
 const { signout, signup } = require("../controllers/auth");
 
 //routers
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("NAME SHOULD BE GREATER THAN 3 CHARS"),
+    check("email").isEmail().withMessage("INVALID EMAIL"),
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 //export statement
