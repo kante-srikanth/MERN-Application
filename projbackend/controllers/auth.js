9ed77fcdc6058450f9946bf9e35fd5b1e6879ae4
Srikanth kante <kantesrikanth@gmail.com>
2020-05-07 23:02:25 +0530

    custom middlewares

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -52,5 +52,34 @@ exports.signin = (req, res) => {
 
 //signout
 exports.signout = (req, res) => {
-  res.json({ message: "user signout success" });
+  res.clearCookies("token");
+  res.json({ message: "user signout success..." });
+};
+
+//protected routes
+
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth",
+});
+
+//custom middlewares
+
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "ACCESS DENIED",
+    });
+  }
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "YOU ARE NOT ADMIN, ACCESS DENIED ",
+    });
+  }
+  next();
 };
