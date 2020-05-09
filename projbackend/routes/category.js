ece066d616cc1deecbb55b93fdb267b11f949c37
Srikanth kante <kantesrikanth@gmail.com>
2020-05-09 14:00:02 +0530

    category route

---
+++ projbackend/routes/category.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getCategoryById,
+  createCategory,
+  getCategory,
+  getAllCategories,
+  updateCategory,
+  removeCategory,
+} = require("../controllers/category");
+const { getUserById } = require("../controllers/user");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+
+//Params
+router.param("userId", getUserById);
+router.param("categoryId", getCategoryById);
+
+//Routers
+router.post(
+  "/category/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createCategory
+);
+
+router.get("/category/:categoryId", getCategory);
+router.get("/categories/", getAllCategories);
+
+router.put(
+  "/category/:categoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateCategory
+);
+
+router.delete(
+  "/category/:categoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  removeCategory
+);
+module.exports = router;
