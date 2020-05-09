069a73c67f9109b0a645ec7577ad62ef9e65816b
Srikanth kante <kantesrikanth@gmail.com>
2020-05-09 09:15:25 +0530

    importing userPurchaseList and updateUser

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUser, getUserById } = require("../controllers/user");
+const {
+  getUser,
+  getUserById,
+  updateUser,
+  userPurchaseList,
+} = require("../controllers/user");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 router.param("userId", getUserById);
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+router.get(
+  "/user/orders/:userId",
+  isSignedIn,
+  isAuthenticated,
+  userPurchaseList
+);
 
 module.exports = router;
