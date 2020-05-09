adafcb4561888aed32d4af25d484251300ccbce7
Srikanth kante <kantesrikanth@gmail.com>
2020-05-09 09:14:18 +0530

    updateUser, userPurchaseList, pushOrderInPurchaseList

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Order = require("../models/order");
 
 exports.getUserById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -11,7 +12,62 @@ exports.getUserById = (req, res, next, id) => {
 };
 
 exports.getUser = (req, res) => {
-  //TODO: For password
   req.profile.salt = req.profile.encry_password = req.profile.createdAt = req.profile.updatedAt = undefined;
   return res.json(req.profile);
+};
+
+exports.updateUser = (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err) return res.status(400).json({ error: "Update not successful" });
+      user.salt = user.encry_password = user.createdAt = user.updatedAt = undefined;
+      return res.json(user);
+    }
+  );
+};
+
+exports.userPurchaseList = (req, res) => {
+  Order.find({ user: req.profile._id })
+    .populate("user", "_id name")
+    .exec((err, order) => {
+      if (err) {
+        return res
+          .status(400)
+          .json({ error: "No products found in this account" });
+      }
+      return res.json(order);
+    });
+};
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+  let purchases = [];
+  req.body.order.products.forEach((product) => {
+    purchases.push({
+      _id: product._id,
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      quantity: product.quantity,
+      amount: req.body.order.amount,
+      transaction_id: req.body.order.transaction_id,
+    });
+  });
+
+  //Store in DB
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $push: { purchases: purchases } },
+    { new: true },
+    (err, purchases) => {
+      if (err) {
+        return res
+          .status(400)
+          .json({ error: "NOT ABLE TO UPDATE THE PURCHASE LIST" });
+      }
+      next();
+    }
+  );
 };
