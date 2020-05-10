034a82d3dc62ceae60437d1d45d269dd816b7e67
Srikanth kante <kantesrikanth@gmail.com>
2020-05-10 19:25:15 +0530

    admin route and private route

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./core/Home";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import UserDashboard from "./user/UserDashBoard";
+import AdminDashboard from "./user/AdminDashBoard";
 
 const Routes = () => {
   return (
@@ -11,6 +15,8 @@ const Routes = () => {
         <Route path="/" exact component={Home}></Route>
         <Route path="/signup" exact component={Signup}></Route>
         <Route path="/signin" exact component={Signin}></Route>
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
+        <PrivateRoute path="/user/dashboard" exact component={UserDashboard} />
       </Switch>
     </Router>
   );
