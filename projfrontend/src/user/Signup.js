c2542db3f09579bded2a2997fad3ab9209951490
Srikanth kante <kantesrikanth@gmail.com>
2020-05-10 13:10:19 +0530

    new sign up  component

---
+++ projfrontend/src/user/Signup.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Base from "../core/Base";
+
+const Signup = () => {
+  const SignUpForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Username</label>
+              <input className="form-control" type="text" />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input className="form-control" type="email" />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input className="form-control" type="password" />
+            </div>
+            <button type="submit" className="btn btn-success btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <Base title="Sign up page" description="This is signup page">
+      {SignUpForm()}
+    </Base>
+  );
+};
+
+export default Signup;
