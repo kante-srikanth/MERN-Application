115b975c4dc3abf19e8d6aefe51f168ecf74fde9
Srikanth kante <kantesrikanth@gmail.com>
2020-05-10 13:08:36 +0530

    routes component

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,12 +1,16 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./core/Home";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home}></Route>
+        <Route path="/signup" exact component={Signup}></Route>
+        <Route path="/signin" exact component={Signin}></Route>
       </Switch>
     </Router>
   );
