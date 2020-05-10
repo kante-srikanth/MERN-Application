77b043360d05c2afcc4e17255e81c9b8bfb0db1c
Srikanth kante <kantesrikanth@gmail.com>
2020-05-10 13:08:59 +0530

    base component template

---
+++ projfrontend/src/core/Base.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Menu from "./Menu";
+const Base = ({
+  title = "My Title",
+  description = "My Description",
+  className = "bg-dark text-white p-4",
+  children,
+}) => (
+  <div>
+    <Menu />
+    <div className="container-fluid">
+      <div className="jumbotron text-white bg-dark text-center">
+        <h2 className="display-4">{title}</h2>
+        <p className="lead">{description}</p>
+      </div>
+      <div className={className}>{children}</div>
+    </div>
+    <footer className="footer bg-dark mt-auto py-3">
+      <div className="container-fluid bg-success text-white text-center py-3">
+        <h4>If you got any question, feel free to reach out</h4>
+        <button className="btn btn-warning btn-lg">Contact Us</button>
+      </div>
+      <div className="container">
+        <span className="text-muted">An amazing MERN boot camp</span>
+      </div>
+    </footer>
+  </div>
+);
+export default Base;
