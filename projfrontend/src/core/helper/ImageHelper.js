b99d4683c9e0e0b013c135ec09b5f85a0a0aebbb
kante-srikanth <kantesrikanth@gmail.com>
2020-10-10 20:03:42 +0530

    added cart item css

--- projfrontend/src/core/helper/ImageHelper.js
+++ projfrontend/src/core/helper/ImageHelper.js
@@ -6,7 +6,7 @@ const ImageHelper = ({ product }) => {
     ? `${API}/product/photo/${product._id}`
     : `https://images.pexels.com/photos/3561339/pexels-photo-3561339.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`;
   return (
-    <div className="rounded border border-success p-2">
+    <div className="cart-item rounded border border-success p-2">
       <img
         src={imageurl}
         alt="photo"
